57343a19ddb6dfc36bc741561612864690b334b6
Bob Gregory <bob@huddle.net>
2014-08-06 21:25:15 +0100

    Stoopid resharper

---
+++ Baduk/Library1.fs
@@ -0,0 +1,15 @@
+﻿namespace Fuseki
+
+type Board (?height: int, ?width: int) =
+
+    let _height = match height with 
+        | Some h -> h
+        | None -> 19
+
+    let _width = match width with 
+        | Some w -> w
+        | None -> _height
+
+    member x.Groups = []
+
+
---
+++ Goban.Test/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Machine.Specifications;
+
+namespace Goban.Test
+{
+    public class When_creating_an_empty_board
+    {
+        Because we_create_a_new_board = () =>
+            The_board = new Board();
+    }
+}
