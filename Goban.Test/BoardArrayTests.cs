6192bbe4d03444796413b18f299a10e93d0cd766
Bob Gregory <bob@huddle.net>
2014-08-07 15:56:14 +0100

    Board array works

---
+++ Goban.Test/BoardArrayTests.cs
@@ -0,0 +1,219 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Machine.Specifications;
+
+namespace Goban.Test
+{
+    public class When_initializing_a_one_by_one_board
+    {
+        Because we_create_a_one_by_one_board = () =>
+            The_board = new BoardArray(1, 1);
+
+        /// <remarks>
+        /// A la gnugo and tesuji we create an array with padding around the edges
+        /// A one element board expands to 
+        /// 
+        /// oo
+        /// ox
+        /// ooo
+        /// 
+        /// where o are Grey blocks and x are empty.
+        /// </remarks>
+        It should_create_a_board_with_padding_around_the_top_left_and_bottom = () =>
+            The_board.Elements.Length.ShouldEqual(7);
+
+        It should_contain_a_single_empty_element = ()
+            => The_board.Elements.Count(x => x == State.Empty).ShouldEqual(1);
+
+        It should_place_the_empty_element_in_the_4th_position_of_the_array = () =>
+            The_board.Elements[3].ShouldEqual(State.Empty);
+
+        It should_return_the_coords_for_the_empty_point = () =>
+            The_board.Coordinates(3).ShouldEqual(new Point(0, 0));
+
+        It should_return_the_correct_index_for_the_empty_point = () =>
+            The_board.Index(new Point(0, 0)).ShouldEqual(3);
+
+        It should_return_no_neighbours_for_the_empty_point = () =>
+            The_board.NeighboursOf(new Point(0, 0)).ShouldBeEmpty();
+
+        private static BoardArray The_board;
+    }
+
+
+    public class When_initializing_a_two_by_one_board
+    {
+        Because we_create_a_two_by_one_board = () =>
+            The_board = new BoardArray(2, 1);
+
+        /// <remarks>
+        /// A la gnugo and tesuji we create an array with padding around the edges
+        /// A 2 * 1 element board expands to 
+        /// 
+        /// ooo
+        /// oxx
+        /// oooo
+        /// 
+        /// where o are Grey blocks and x are empty.
+        /// </remarks>
+        It should_create_a_board_with_padding_around_the_top_left_and_bottom = () =>
+            The_board.Elements.Length.ShouldEqual(10);
+
+        It should_contain_two_empty_elements = ()
+            => The_board.Elements.Count(x => x == State.Empty).ShouldEqual(2);
+
+        It should_place_the_first_empty_element_in_the_5th_position_of_the_array = () =>
+            The_board.Elements[4].ShouldEqual(State.Empty);
+
+        It should_place_the_second_empty_element_in_the_6th_position_of_the_array = () =>
+            The_board.Elements[5].ShouldEqual(State.Empty);
+
+        It should_return_the_coords_for_the_first_empty_point = () =>
+            The_board.Coordinates(4).ShouldEqual(new Point(0, 0));
+
+
+        It should_return_the_coords_for_the_second_empty_point = () =>
+            The_board.Coordinates(5).ShouldEqual(new Point(1, 0));
+
+        It should_return_the_correct_index_for_the_first_empty_point = () =>
+            The_board.Index(new Point(0, 0)).ShouldEqual(4);
+
+        It should_return_the_correct_index_for_the_second_empty_point = () =>
+            The_board.Index(new Point(1, 0)).ShouldEqual(5);
+
+        private static BoardArray The_board;
+    }
+
+    public class When_initializing_a_one_by_two_board
+    {
+        Because we_create_a_two_by_one_board = () =>
+            The_board = new BoardArray(1, 2);
+
+        /// <remarks>
+        /// A la gnugo and tesuji we create an array with padding around the edges
+        /// A 1 * 2 element board expands to 
+        /// 
+        /// oo
+        /// ox
+        /// ox
+        /// ooo
+        /// 
+        /// where o are Grey blocks and x are empty.
+        /// </remarks>
+        It should_create_a_board_with_padding_around_the_top_left_and_bottom = () =>
+            The_board.Elements.Length.ShouldEqual(9);
+
+        It should_contain_two_empty_elements = ()
+            => The_board.Elements.Count(x => x == State.Empty).ShouldEqual(2);
+
+        It should_place_the_first_empty_element_in_the_4th_position_of_the_array = () =>
+            The_board.Elements[3].ShouldEqual(State.Empty);
+
+        It should_place_the_second_empty_element_in_the_6th_position_of_the_array = () =>
+            The_board.Elements[5].ShouldEqual(State.Empty);
+
+        It should_return_the_coords_for_the_first_empty_point = () =>
+            The_board.Coordinates(3).ShouldEqual(new Point(0, 0));
+
+
+        It should_return_the_coords_for_the_second_empty_point = () =>
+            The_board.Coordinates(5).ShouldEqual(new Point(0, 1));
+
+        It should_return_the_correct_index_for_the_first_empty_point = () =>
+            The_board.Index(new Point(0, 0)).ShouldEqual(3);
+
+        It should_return_the_correct_index_for_the_second_empty_point = () =>
+            The_board.Index(new Point(0, 1)).ShouldEqual(5);
+
+        It should_return_the_second_point_as_a_neighbour_of_the_first = () =>
+            The_board.NeighboursOf(new Point(0, 0)).ShouldContainOnly(5);
+
+        It should_return_the_first_point_as_a_neighbour_of_the_second = () =>
+            The_board.NeighboursOf(new Point(0, 1)).ShouldContainOnly(3);
+
+
+        private static BoardArray The_board;
+    }
+
+    /// <summary>
+    /// Maps between 2D coordinates and 1D indices into the Elements array
+    /// Coordinates off the edges are guaranteed to map to valid array indices
+    /// which makes neighbour look ups trivial.
+    /// </summary>
+    internal class BoardArray
+    {
+        private readonly int _width;
+        private readonly int _height;
+
+        public BoardArray(int width, int height)
+        {
+            _width = width;
+            _height = height;
+            Elements = new State[1 + ((height + 2)* (width + 1))];
+            InitBorders();
+        }
+
+        private void InitBorders()
+        {
+            var c = 0;
+            var r = 0;
+            while (c <= _width + 1)
+            {
+                Elements[c++] = State.Grey;
+            }
+
+            while (r ++ < _height)
+            {
+                c += _width;
+                Elements[c++] = State.Grey;
+            }
+
+            while (c < Elements.Length)
+                Elements[c++] = State.Grey;
+
+        }
+
+        public State[] Elements { get; private set; }
+
+        public int Above(int index)
+        {
+            return index - (1 + _width);
+        }
+
+        public int Below(int index)
+        {
+            return index + (1 + _width);
+        }
+
+        public int Right(int index)
+        {
+            return index + 1;
+        }
+
+        public int Left(int index)
+        {
+            return index - 1;
+        }
+
+        public Point Coordinates(int index)
+        {
+            var y = (index/(_width + 1) - 1);
+            var x = (index  %(_width + 1) - 1);
+            return new Point(x, y);
+        }
+
+        public int Index(Point point)
+        {
+            return (_width + 2) + point.Y*(_width + 1) + point.X;
+        }
+
+        public IEnumerable<int> NeighboursOf(Point point)
+        {
+            var pos = Index(point);
+            if (Elements[Above(pos)] != State.Grey) yield return Above(pos);
+            if (Elements[Right(pos)] != State.Grey) yield return Right(pos);
+            if (Elements[Below(pos)] != State.Grey) yield return Below(pos);
+            if (Elements[Left(pos)] != State.Grey) yield return Left(pos);
+        }
+    }
+}
--- Goban.Test/Class1.cs
+++ Goban.Test/Class1.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using CuttingEdge.Conditions;
 using Machine.Specifications;
 
 namespace Goban.Test
@@ -11,5 +10,217 @@ namespace Goban.Test
     {
         Because we_create_a_new_board = () =>
             The_board = new Board();
+
+        It has_no_groups = () =>
+            The_board.Groups.ShouldBeEmpty();
+
+        It should_be_black_to_move = () =>
+            The_board.ToPlay.ShouldEqual(Colour.Black);
+
+        static Board The_board;
+    }
+
+    public class When_setting_the_only_point_on_a_one_by_one_board
+    {
+        Establish context = () => The_board = new Board(3, 3);
+
+        Because we_set_the_only_available_stone = () => The_board.Set(0, 0, Colour.White);
+
+        It should_have_the_correct_colour = () => 
+            The_board.Get(0, 0).State.ShouldEqual(State.White);
+        
+        private static Board The_board;
+    }
+
+    public class When_getting_coords_for_a_silly_board_size
+    {
+        Establish context = () =>
+            the_board = new Board(1, 7);
+
+        It should_return_the_correct_coordinates_for_the_first_non_grey_position = () =>
+            the_board.Coordinates(4).ShouldEqual(new Point(0, 0));
+
+
+
+        private static Board the_board;
+    }
+
+    public class When_we_set_a_stone_in_a_valid_position
+    {
+        Establish context = () =>
+            The_board = new Board();
+
+        Because we_set_a_stone = () =>
+            The_board.Set(9, 9, Colour.Black);
+
+        It should_have_one_group = () =>
+            The_board.Groups.Count().ShouldEqual(1);
+
+        It should_return_the_correct_colour_for_the_position = () =>
+            The_board.Get(9, 9).State.ShouldEqual(State.Black);
+
+        It should_have_the_correct_move_number_for_the_position = () =>
+            The_board.Get(9, 9).MoveNumber.ShouldEqual(1);
+
+        private static Board The_board;
+    }
+
+    internal enum State
+    {
+        Grey = 1,
+        Empty = 0,
+        Black =2 ,
+        White = 3
+    }
+
+    internal enum Colour
+    {
+        Empty = 0,
+        Black =2,
+        White =3
+    }
+
+
+    internal class Board
+    {
+        private readonly int _width;
+        private readonly int _height;
+        private int _nextGroup = 1;
+        private State[] _board;
+        private int[] _groups;
+        private int[] _moveNumbers;
+        private int _lastMove = 0;
+
+        public Board(int width = 19, int height = 19)
+        {
+            Condition.Requires(width, "width").IsGreaterThan(0).IsLessThan(35);
+            Condition.Requires(height, "height").IsGreaterThan(0).IsLessThan(35);
+            _width = width;
+            _height = height;
+            ToPlay = Colour.Black;
+            int size = ((height + 2) * (width+ 1) + 1);
+            _board = InitBounds(new State[size]);
+            _moveNumbers = new int[size];
+            _groups = new int[size];
+        }
+
+        private State[] InitBounds(State[] p0)
+        {
+            Condition.Requires(p0).IsNotNull().IsLongerOrEqual(9);
+            // set first row
+            int p;
+
+            for (p = 0; p < 2 + _width; p++)
+            {
+                p0[p] = State.Grey;
+            }
+
+            for (var row = 0; row < _height; row ++)
+            {
+                p0[p] = State.Grey;
+                p += _width+1;
+                p0[p++] = State.Grey;
+            }
+
+            for (var col = 0; col <= _width+1; col ++)
+            {
+                p0[p + col] = State.Grey;
+            }
+            return p0;
+        }
+
+        public IEnumerable<Group> Groups
+        {
+            get { yield break; }
+        }
+
+        public Colour ToPlay { get; private set; }
+
+        public void Set(int x, int y, Colour state)
+        {
+            Condition.Requires(x).IsGreaterOrEqual(0).IsLessThan(_width);
+            Condition.Requires(y).IsGreaterOrEqual(0).IsLessThan(_height);
+            var pos = Index(x, y);
+            _board[pos] = (State)state;
+            _moveNumbers[pos] = (++_lastMove);
+        }
+
+        public Stone Get(int x, int y)
+        {
+            Condition.Requires(x).IsGreaterOrEqual(0).IsLessThan(_width);
+            Condition.Requires(y).IsGreaterOrEqual(0).IsLessThan(_height);
+            var pos = Index(x, y);
+            var state = _board[pos];
+            var moveNumber = _moveNumbers[pos];
+            return new Stone(state, moveNumber);
+        }   
+
+        private int Index(int x, int y)
+        {
+            return 4 + (2*x) + (x*y) + (2*y);
+        }
+
+        public Point Coordinates(int idx)
+        {
+            return new Point((idx / (_width + 3 ) -1 ),  ((idx % (_height + 3)) - 1));
+        }
+    }
+
+    internal struct Point
+    {
+        private readonly int _x;
+        private readonly int _y;
+
+        public Point(int x, int y)
+        {
+            _x = x;
+            _y = y;
+        }
+
+        public int X
+        {
+            get { return _x; }
+        }
+
+        public int Y
+        {
+            get { return _y; }
+        }
+
+        public bool Equals(Point other)
+        {
+            return _x == other._x && _y == other._y;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            return obj is Point && Equals((Point) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (_x*397) ^ _y;
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("({0},{1})", _x, _y);
+        }
+    }
+
+    internal class Stone
+    {
+        public Stone(State state, int moveNumber)
+        {
+            State = state;
+            MoveNumber = moveNumber;
+        }
+
+        public State State { get; private set; }
+        public int MoveNumber { get; private set; }
     }
 }
